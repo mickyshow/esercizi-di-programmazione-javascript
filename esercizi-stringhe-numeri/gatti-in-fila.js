6ba7fb9c163d85f770e1827add1030738f89d506
mickyshow <52820939+mickyshow@users.noreply.github.com>
2019-07-12 12:12:50 +0200

    Update gatti-in-fila.js

--- esercizi-stringhe-numeri/gatti-in-fila.js
+++ esercizi-stringhe-numeri/gatti-in-fila.js
@@ -6,14 +6,14 @@
   Restituisca in output:
     - Il numero di file risultanti
     - Nel caso l'ultima fila risulti incompleta indicare il numero di gatti mancanti per completarla (quanti gatti mancano per arrivare a b?)
-
   Esempio:
     Input: numero di gatti = 44, gatti per fila = 6
     Output: numero di file = 8, gatti mancanti = 4
-
-  Consigli:
-  Per l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su),
+  Consigli: Per l'arrotondamento ti consiglio di non usare Math.round (arrotonda all'intero superiore solo da .5 in su),
   utilizza Math.ceil (arrotonda sempre all'intero superiore).
 
   http://www.imparareaprogrammare.it
 */
+
+var cats = 44;
+
